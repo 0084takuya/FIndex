3813df955bf497d324eb7114ddd405eef9664288
Tatsuya Ishii <tishii2479@gmail.com>
2020-09-24 22:44:03 +0900

    Player Image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,5 +8,16 @@ module ApplicationHelper
     def format_date(year, month, day)
         return Date.new(year.to_i, month.to_i, day.to_i) if Date.valid_date?(year.to_i, month.to_i, day.to_i)
         return Date.new(1899, 12, 31)
-    end 
+    end
+
+    def player_image(image_name)
+        return image_tag "system/person", class: "rounded-img" if image_name == "" 
+        
+        image_path = "player/#{image_name}.jpg"
+        if File.exist? "#{Rails.root}/app/assets/images/player/#{image_name}.jpg"
+            return image_tag image_path, class: "rounded-img"
+        else
+            return image_tag "system/person", class: "rounded-img"
+        end
+    end
 end
