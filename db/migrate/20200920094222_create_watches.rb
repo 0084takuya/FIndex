e5e5571fde09e3d37a1d9720f5aa16fb9da327e1
Tatsuya Ishii <tishii2479@gmail.com>
2020-09-26 17:07:01 +0900

    Fix MigrationFile

--- db/migrate/20200920094222_create_watches.rb
+++ db/migrate/20200920094222_create_watches.rb
@@ -1,4 +1,4 @@
-class CreateWatches < ActiveRecord::Migration[7.0]
+class CreateWatches < ActiveRecord::Migration[6.0]
   def change
     create_table :watches do |t|
       t.references :user, null: false
